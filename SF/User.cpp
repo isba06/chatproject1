93d00debe38b748783c45e71e613454f3d8b21ff
ismail <www.ismail.com@mail.ru>
2021-03-26 20:03:16 +0300

    дополнение

--- SF/User.cpp
+++ SF/User.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include "User.h"
 
-class User
+void User::inMessage(std::string in_message)
 {
+	_in_message[_countInMessage++] = in_message;
+}
 
-};
+bool User::cmp(std::string username, std::string password)
+{
+	if ((_username == username) && (password == _password))
+		return true;
+	else return false;
+
+}
+bool checkAvailability(std::string username);
--- SF/User.h
+++ SF/User.h
@@ -6,18 +6,22 @@ class User
 private:
 	std::string _id;
 	std::string _name;
-	std::string _surname;
+	std::string _username;
+	std::string _password;
+	int _countInMessage; //порядковый номер для входящего сообщения
+	int _countOutMessage;
 	std::vector<std::string> _in_message;
 	std::vector<std::string> _out_message;
 public:
-	User(std::string id, std::string name, std::string surname) : _id(id), _name(name), _surname(surname)
+	User(std::string id, std::string name, std::string username, std::string password) : _id(id), _name(name), _username(username), _password(password), _countInMessage(0), _countOutMessage(0)
 	{
 
 	}
-	User(std::string id, std::string name) : _id(id), _name(name)
-	{
 
-	}
 	~User() = default;
-
+	
+	void inMessage(std::string);
+	void OutMessage(std::string);
+	bool cmp(std::string username, std::string password);
+	bool checkAvailability(std::string username); 
 };
